e528492a6d6314ec48ccf45a383e5e4ff2e78a5a
Ryosuke Kikuchi <r-kikuchi@dip-net.co.jp>
2020-11-04 22:10:53 +0900

    change log

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request'),
 const bodyParser = require('body-parser')
 
 router.use(bodyParser());
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-	console.log(req.body.challenge)
+	console.log(req.body)
   res.status(200).send(req.body.challenge);
 });
+
+
+
+function callSendAPI(messageData, callback) {
+	const messageData = {
+		"channel": "",
+		"text": "Hello world"
+	}
+
+	request({
+		uri: 'https://slack.com/api/chat.postMessage',
+		qs: { access_token: token },
+		method: 'POST',
+		json: messageData
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+	  		if(callback != null) callback(null)
+		} else {
+	  	if(callback != null) callback(error)
+		}
+	});
+}
 
 module.exports = router;

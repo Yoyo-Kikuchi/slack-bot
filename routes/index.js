7371615ca91834fb9fbe2901e2e7d3843b29c3f0
Ryosuke Kikuchi <r-kikuchi@dip-net.co.jp>
2020-11-04 21:20:35 +0900

    add bodypaser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser')
+
+router.use(bodyParser());
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.status(200).send(req.challenge);
+	// console.log(req.body.challenge)
+  res.status(200).send(req.body.challenge);
 });
 
 module.exports = router;

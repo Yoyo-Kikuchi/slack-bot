68e26828734350b33ace9dca0b3d6d81c5babfd4
Ryosuke Kikuchi <r-kikuchi@dip-net.co.jp>
2020-11-04 22:15:09 +0900

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const request = require('request'),
+const request = require('request')
 const bodyParser = require('body-parser')
 
 router.use(bodyParser());

e76d2b85de9d6b0b47215e6d12c88206ddb4eba7
Ryosuke Kikuchi <r-kikuchi@dip-net.co.jp>
2020-11-04 23:31:10 +0900

    implment request

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,33 @@ var express = require('express');
 var router = express.Router()
 const request = require('request')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 router.use(bodyParser());
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-	console.log(req.body)
+router.post('/', async (req, res, next) => {
+	console.log(req)
+	await callSendAPI(req.body)
   	res.status(200);
 });
 
 
 
-// function callSendAPI(messageData, callback) {
-// 	const messageData = {
-// 		"channel": "",
-// 		"text": "Hello world"
-// 	}
-// z
-// 	request({
-// 		uri: 'https://slack.com/api/chat.postMessage',
-// 		qs: { access_token: token },
-// 		method: 'POST',
-// 		json: messageData
-// 	}, function (error, response, body) {
-// 		if (!error && response.statusCode == 200) {
-// 	  		if(callback != null) callback(null)
-// 		} else {
-// 	  	if(callback != null) callback(error)
-// 		}
-// 	});
-// }
+async function callSendAPI(message) {
+	const messageData = {
+		"channel": message.channel,
+		"text": message.text
+	}
+
+	return await request({
+		uri: 'https://slack.com/api/chat.postMessage',
+		auth: {
+    		'bearer':process.env.TOKEN
+    	},
+		method: 'POST',
+		json: messageData
+	});
+}
 
 module.exports = router;

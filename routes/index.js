64c4bb2ec2fd7fe1856bede703a45cfabc54d116
Ryosuke Kikuchi <r-kikuchi@dip-net.co.jp>
2020-11-04 20:48:27 +0900

    modify responce

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send("Hello World");
+  res.status(200).send(req);
 });
 
 module.exports = router;
